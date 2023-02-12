0f55f6454e90325fa76bab750509c5e2d9086ef6
fpaiva <felipe.paiva@nttdata.com>
2023-02-12 12:41:28 -0300

    new route

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
     // baseURL: "http://localhost:3000/api",
-    baseURL: "https://test-sap-back-nest-chatty-mongoose-um.cfapps.us10-001.hana.ondemand.com/api",
+    baseURL: "https://test-sap-back-nest.cfapps.us10-001.hana.ondemand.com/api",
 };
